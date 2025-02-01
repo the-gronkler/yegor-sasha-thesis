a72c10071035c0ac4ec0b4389620ab3ae7398404
the-gronkler <alexsvirin1337@gmail.com>
2025-02-01 14:53:39 +0100

    add 'mfs' artisan command
    runs 'migrate:fresh --path=database/migrations/* --seed', to use type 'php artisan mfs'
    
    add 'mfs' artisan command
    
    runs 'migrate:fresh --path=database/migrations/* --seed', to use type 'php artisan mfs'

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command(
+    'mfs',
+    fn() =>  Artisan::call('migrate:fresh', [
+        '--path' => 'database/migrations/*',
+        '--seed' => true,
+        '--no-interaction' => true,
+        ], new ConsoleOutput()
+    )
+)->describe('Runs migrate:fresh with seed, including migrations from all subdirectories');
