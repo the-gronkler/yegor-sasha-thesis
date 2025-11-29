aed4822237563cc11a8ec87d1cfd145c81f6365f
the-gronkler <alexsvirin1337@gmail.com>
2025-11-29 20:11:03 +0100

    fix: remove hasApiTokens from user model, since Sanctum is not installed

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
