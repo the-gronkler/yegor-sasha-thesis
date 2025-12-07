4d3ad56428ad77e73ce6b60161421eef09b8724d
the-gronkler <alexsvirin1337@gmail.com>
2025-12-07 19:15:00 +0100

    chore: format php files

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -13,18 +13,18 @@ class ImageFactory extends Factory
      * Array of real image URLs for seeding.
      */
     protected static array $realImageUrls = [
-    'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg',
-    'https://www.foodiesfeed.com/wp-content/uploads/2023/09/fresh-vegetables.jpg',
-    'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/06/crispy-fried-chicken-drumstick-in-hot-oil.webp',
-    'https://www.foodiesfeed.com/wp-content/uploads/2023/05/pizza-salami.jpg',
-    'https://www.foodiesfeed.com/wp-content/uploads/2023/12/pizza-salami-close-up.jpg',
-    'https://www.foodiesfeed.com/wp-content/uploads/2023/05/freshly-prepared-beef-steak-with-vegetables.jpg',
-    'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/06/grilled-meat-wrap-with-fresh-vegetables-and-fries.webp',
-    'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/01/flavorful-shrimp-feast-with-lemon-and-corn.png',
-    'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/08/flavorful-tacos-with-guacamole-and-beer.webp',
-    'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/08/fresh-tacos-being-assembled-in-busy-kitchen.webp',
-    'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/06/colorful-sushi-rolls-floating-in-air.webp',
-];
+        'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg',
+        'https://www.foodiesfeed.com/wp-content/uploads/2023/09/fresh-vegetables.jpg',
+        'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/06/crispy-fried-chicken-drumstick-in-hot-oil.webp',
+        'https://www.foodiesfeed.com/wp-content/uploads/2023/05/pizza-salami.jpg',
+        'https://www.foodiesfeed.com/wp-content/uploads/2023/12/pizza-salami-close-up.jpg',
+        'https://www.foodiesfeed.com/wp-content/uploads/2023/05/freshly-prepared-beef-steak-with-vegetables.jpg',
+        'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/06/grilled-meat-wrap-with-fresh-vegetables-and-fries.webp',
+        'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/01/flavorful-shrimp-feast-with-lemon-and-corn.png',
+        'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/08/flavorful-tacos-with-guacamole-and-beer.webp',
+        'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/08/fresh-tacos-being-assembled-in-busy-kitchen.webp',
+        'https://www.foodiesfeed.com/wp-content/uploads/ff-images/2025/06/colorful-sushi-rolls-floating-in-air.webp',
+    ];
 
     public function definition(): array
     {
