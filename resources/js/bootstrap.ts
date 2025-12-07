a1b534a7a8df46a7dd595badf2705ec08d871878
the-gronkler <alexsvirin1337@gmail.com>
2025-12-07 20:32:52 +0100

    fix merge

---
+++ resources/js/bootstrap.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { route as routeFn } from "ziggy-js";
+import { Ziggy } from "./ziggy";
+
+window.axios = axios;
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// @ts-ignore
+window.route = (
+    name?: string,
+    params?: any,
+    absolute?: boolean,
+    config?: any
+) => {
+    if (name === undefined) {
+        return routeFn(undefined, undefined, undefined, config || Ziggy);
+    }
+    return routeFn(name, params, absolute, config || Ziggy);
+};
+window.Ziggy = Ziggy;
--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,5 +1,12 @@
+import { AxiosInstance } from "axios";
 import { route as routeFn } from "ziggy-js";
+import { Config } from "ziggy-js";
 
 declare global {
+    interface Window {
+        axios: AxiosInstance;
+        route: typeof routeFn;
+        Ziggy: Config;
+    }
     var route: typeof routeFn;
 }
