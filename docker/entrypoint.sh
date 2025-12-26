c67b10abfb5f1d70374a9440caa4b21e63b2d747
the-gronkler <alexsvirin1337@gmail.com>
2025-12-26 13:54:51 +0100

    feat: specify migration path in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -25,7 +25,7 @@ php artisan storage:link || true
 
 # Run migrations
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --force --path="database/migrations/*"
 
 # Cache config/routes/views for production
 echo "Caching configuration..."
