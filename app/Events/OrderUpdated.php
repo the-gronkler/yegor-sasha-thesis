0212352a61d0c6017b69b5412df60f1ac3e15ce9
the-gronkler <alexsvirin1337@gmail.com>
2026-01-06 21:12:37 +0100

    feat(FE): restore deleted broadcasting and order update files

---
+++ app/Events/OrderUpdated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Order $order)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('order.'.$this->order->id),
+            new Channel('restaurant.'.$this->order->restaurant_id),
+            new Channel('user.'.$this->order->customer_user_id),
+        ];
+    }
+}
