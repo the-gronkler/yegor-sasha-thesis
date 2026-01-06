81bbc1d6129a9e0dd147cf3cd10398fda9cd8158
the-gronkler <alexsvirin1337@gmail.com>
2026-01-06 14:28:19 +0100

    chore: fix some stuff after cde review

--- routes/channels.php
+++ routes/channels.php
@@ -9,19 +9,23 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('order.{orderId}', function ($user, $orderId) {
-    $order = Order::with('restaurant')->find($orderId);
+    $order = Order::find($orderId);
     if (! $order) {
         return false;
     }
 
     // Allow customer or employees of the restaurant
+    $employeeRestaurantId = $user->employee?->restaurant_id;
+
     return (int) $user->id === (int) $order->customer_user_id ||
-        ($user->employee && (int) $user->employee->restaurant_id === (int) $order->restaurant_id);
+        ($employeeRestaurantId !== null && (int) $employeeRestaurantId === (int) $order->restaurant_id);
 });
 
 Broadcast::channel('restaurant.{restaurantId}', function ($user, $restaurantId) {
     // Allow employees of the restaurant
-    return $user->employee && (int) $user->employee->restaurant_id === (int) $restaurantId;
+    $employeeRestaurantId = $user->employee?->restaurant_id;
+
+    return $employeeRestaurantId !== null && (int) $employeeRestaurantId === (int) $restaurantId;
 });
 
 Broadcast::channel('user.{userId}', function ($user, $userId) {
