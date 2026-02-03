341955521cc5fedc364c0e1e3e275d5141e164c9
the-gronkler <alexsvirin1337@gmail.com>
2026-02-03 22:20:14 +0100

    fix/migrations/change-image-column-type

--- database/migrations/images/1000_00_00_000010_create_images_table.php
+++ database/migrations/images/1000_00_00_000010_create_images_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->binary('image')->nullable();
+            $table->string('image', 500)->nullable();
             $table->string('description')->nullable();
             $table->unsignedBigInteger('restaurant_id')->nullable();
             $table->unsignedBigInteger('menu_item_id')->nullable();
@@ -24,8 +23,6 @@ return new class extends Migration
             $table->foreign('menu_item_id')->references('id')->on('menu_items')->onDelete('set null');
             $table->timestamps();
         });
-
-        DB::statement('ALTER TABLE images MODIFY COLUMN image MEDIUMBLOB');
     }
 
     /**
